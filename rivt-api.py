cc463688e67faa9c8dcae54411260b63c4f0e61e
R D <dedmanruben@gmail.com>
2022-08-09 10:50:57 -0500

    collab with Liam

---
+++ rivt-api.py
@@ -0,0 +1,45 @@
+import requests
+import json
+
+url = "https://wsgx.cisco.com/wwchannels/services/external/getData"
+
+payload = json.dumps({
+  "service": "getRessellerData",
+  "ccoId": "rubend@synnex.com",
+  "profileId": "975",
+  "entity": [
+    {
+      "sourceName": "COMSTOR",
+      "resellerAccNo": "",
+      "soldToName": "",
+      "partnerName": "24/7 NETWORKS LLC",
+      "city": "",
+      "state": "",
+      "postalCode": "",
+      "country": "USA",
+      "specialization": "",
+      "certification": "",
+      "authorization": "",
+      "theaterCode": "USA",
+      "partnerType": "",
+      "partnerSiteid": "",
+      "pageNumber": 1,
+      "pageSize": 50
+    }
+  ]
+})
+headers = {
+  'Content-Type': 'application/json',
+  'Authorization': 'Basic ZGlzdGlfc2VydmljZXMuZ2VuOkQhc3RyaWJ1dDFu',
+  'Cookie': 'a79f8307c5db34ee3f6ffd933dd657a9=bbafdea3f4a8e5b5450320700f656451; d9c4bd7d7cdfdd17f7f220ce2c1d6546=3bd8e4cf58f23dd67066f9e5cd4cb59a'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+my_response = json.loads(payload)
+
+# ToDo: Print out Every partnerNameBeGeoName,certification returned in the object
+
+Output:
+24/7 NETWORKS LLC,REGISTERED
+24/7 NETWORKS LLC,PREMIER
